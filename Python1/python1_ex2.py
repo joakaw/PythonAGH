f219acd3cb86a59e5e204071dd8454a277cc2397
joakaw <joannakawalko22@gmail.com>
2017-11-30 22:28:30 +0100

    Python1: ex1 - ex3

---
+++ Python1/python1_ex2.py
@@ -0,0 +1,3 @@
+
+name, surname, birthDate = input("Enter your name, surname, date of birth: ").split()
+print ('Name: ' + name + '\nSurname: ' + surname + '\nDate of birth: ' + birthDate)
---
+++ Python1/python1_ex3.py
@@ -0,0 +1,4 @@
+import os
+
+number_of_files = len(os.listdir("D:\Python_Exercise_final"))
+print ("Number of files in folder: ", number_of_files)
